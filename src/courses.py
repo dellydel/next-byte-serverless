c58f7a397540d4803034679a4ee31cfff6377f8d
Delvin Hall <dlvhall@gmail.com>
2025-02-09 10:44:14 -0600

    update to courses logic

--- src/courses.py
+++ src/courses.py
@@ -1,27 +1,34 @@
+# Description: This file contains the logic to retrieve all courses from the database.
+import os
 import boto3
 from src.http_response import create_response
 from botocore.exceptions import ClientError
-from src.utils.unmarshall import deserialize, convert_decimal
+from src.http_response import create_response
+from src.utils.unmarshall import deserialize
 
 dynamodb = boto3.client('dynamodb')
+tableName=os.environ.get("COURSES_TABLE")
 
-def getRegistrationByEmail(email):
+def getAllCourses():
   response = dynamodb.scan(
-     TableName=os.environ.get("REGISTRATIONS_TABLE"),
-     FilterExpression="emailLower = :emailLower OR email = :email",
-     ExpressionAttributeValues={
-      ":emailLower": { "S": email.lower() },
-      ":email": { "S": email },
-    },
-    ProjectionExpression="amount, created, course_name",
-    )
+    TableName=tableName,
+  )
   try:
-    payments = deserialize(response)
-    amounts = list(map(lambda obj: {
-      'date': datetime.datetime.fromtimestamp(int(obj.get("created")) / 1000).strftime('%Y-%m-%d %H:%M:%S'),
-      'amount': convert_decimal(obj.get("amount")),
-      'name': obj.get("course_name"),
-    }, payments.get("Items")))
-    return create_response(200, amounts)
-  except: 
-    raise ClientError("Error when attempting to retrieve registrations.")
+      courses = deserialize(response)
+      return create_response(200, courses.get("Items"))
+  except:
+      raise ClientError("Error when attempting to retrieve courses.")
+  
+def get_courses_by_id(course_ids):
+    response = dynamodb.scan(
+        TableName=tableName,
+    )
+    try:
+        courses = deserialize(response)
+        registered_courses = [
+            course for course in courses.get("Items", [])
+            if course.get("id") in course_ids
+        ]
+        return create_response(200, registered_courses)
+    except ClientError as e:
+        raise ClientError(f"Error when attempting to retrieve courses: {str(e)}")
--- src/courses_handler_get.py
+++ src/courses_handler_get.py
@@ -1,11 +1,14 @@
 from src.error_handler import handle_client_error
 from botocore.exceptions import ClientError
-from src.registration import getRegistrationByEmail 
+from src.registration import getRegistrationByEmail
+from src.courses import getAllCourses 
 
 def handler(event, _):
   try:
-    query_params = event.get('queryStringParameters', {})
-    email = query_params.get('email')
-    return  getRegistrationByEmail(email)
+    if not event.get('queryStringParameters') or not event.get('queryStringParameters').get('email'):
+      return getAllCourses()
+    else:
+      email = event.get('queryStringParameters').get('email')
+      return getRegistrationByEmail(email)   
   except ClientError as err:
       handle_client_error(err)
