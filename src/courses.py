403720f68e38b7dab2ca75c428b71a176577a25e
Delvin <dlvhall@gmail.com>
2026-03-06 11:33:39 -0600

    adding back get courses by id

--- src/courses.py
+++ src/courses.py
@@ -5,6 +5,9 @@ import logging
 from src.http_response import create_response
 from botocore.exceptions import ClientError
 from src.http_response import create_response
+from boto3.dynamodb.types import TypeDeserializer
+
+deserializer = TypeDeserializer()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO) 
@@ -31,4 +34,28 @@ def get_course_by_id(course_id):
             return create_response(200, response['Item'])
         return create_response(404, {"message": "Course not found"})
     except ClientError as e:
+        return create_response(500, f"Error retrieving courses: {str(e)}")
+       
+def get_courses_by_id(course_ids):
+    try:
+        if not course_ids:
+            return create_response(200, [])
+        
+        dynamodb_client = boto3.client('dynamodb')
+        keys = [{'id': {'S': str(course_id)}} for course_id in course_ids]
+        
+        response = dynamodb_client.batch_get_item(
+            RequestItems={
+                os.environ.get("COURSES_TABLE"): {
+                    'Keys': keys
+                }
+            }
+        )
+        
+        items = response.get('Responses', {}).get(os.environ.get("COURSES_TABLE"), [])
+        courses = [{k: deserializer.deserialize(v) for k, v in item.items()} for item in items]
+        
+        return create_response(200, courses)
+    except ClientError as e:
+        logger.error(f"Error retrieving courses: {str(e)}")
         return create_response(500, f"Error retrieving courses: {str(e)}")
